d18c41d83e92ba8de3ee53cac0290a09dda96919
Scott Jehl <scottjehl@gmail.com>
2015-09-24 09:28:18 -0500

    cleanup/refactor pass

--- src/layersnap.js
+++ src/layersnap.js
@@ -11,103 +11,137 @@ SVG Build Animations
 		$( w.document.documentElement ).addClass( "svg-supported" );
 	}
 
+	// strings
+	var svgEl = "svg";
+	var childGroups = svgEl + " > g[id]";
+
+	// ID chunker regexps
+	var regDuration = /(^|\s|_)duration[\-_]+([\d]+)/;
+	var regDelay = /(^|\s|_)delay[\-_]+([\d]+)/;
+	var regToggle = /(^|\s|_)toggle\-([^\s_$]+)/;
+
+	// interactivity
+	var interactiveAttr = "data-layersnap-interact";
+	var activeGroupClass = "layersnap-toggle-active";
+	var activeGroupID = "activegroup";
+	var activeGroupSel = "g[id*=" + activeGroupID + "]";
+	var toggleClass = "layersnap-toggle-hide";
+	var toggleTriggerElementClass = "layersnap-toggle";
+	var interactivitySet = "bound";
+
+	var transitions = {
+		"rotate-right": function( el, duration, bbox ){
+			el.attr( {transform: "r-30"} );
+			el.animate({ transform: "r0," + bbox.cx + ',' + bbox.cy, opacity: 1 }, duration, mina.easeOut );
+		},
+		"rotate-left": function( el, duration, bbox ){
+			el.attr( {transform: "r30"} );
+			el.animate({ transform: "r0," + bbox.cx + ',' + bbox.cy, opacity: 1 }, duration, mina.easeOut );
+		},
+		"fade": function( el, duration ){
+			el.animate({ opacity: 1 }, duration, mina.easeOut );
+		},
+		"scale-up": function( el, duration, bbox ){
+			el.attr( {transform: "s.7"} );
+			el.animate({ opacity: 1, transform: "s1," + bbox.cx + ',' + bbox.cy }, duration, mina.easeOut );
+		},
+		"scale-down": function( el, duration, bbox ){
+			el.attr( {transform: "s1.3"} );
+			el.animate({ opacity: 1, transform: "s1," + bbox.cx + ',' + bbox.cy }, duration, mina.easeOut );
+		},
+		"pop": function( el, duration, bbox ){
+			el.attr( {transform: "s.7"} );
+			el.animate({ opacity: 1, transform: "s1," + bbox.cx + ',' + bbox.cy }, duration, mina.elastic );
+		},
+		"drift-up": function( el, duration ){
+			el.attr( {transform: "translate(0,30)"} );
+			el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
+		},
+		"drift-down": function( el, duration ){
+			el.attr( {transform: "translate(0,-30)"} );
+			el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
+		},
+		"drift-left": function( el, duration ){
+			el.attr( {transform: "translate(30,0)"} );
+			el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
+		},
+		"drift-right": function( el, duration ){
+			el.attr( {transform: "translate(-30,0)"} );
+			el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
+		},
+		"slide-up": function( el, duration, bbox ){
+			el.attr( {transform: "translate(0," + bbox.height + ")"} );
+			el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
+		},
+		"slide-down": function( el, duration, bbox ){
+			el.attr( {transform: "translate(0," + -bbox.height + ")"} );
+			el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
+		},
+		"slide-left": function( el, duration, bbox ){
+			el.attr( {transform: "translate(0," + bbox.width + ")"} );
+			el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
+		},
+		"slide-right": function( el, duration, bbox ){
+			el.attr( {transform: "translate(0," + -bbox.width + ")"} );
+			el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
+		}
+	};
+
+	var runTransition = function( settings ){
+		setTimeout( function(){
+			transitions[ settings.transition ]( settings.el, settings.duration, settings.bbox );
+		}, settings.delay );
+	};
+
 	$.fn.layersnap = function(){
+
 		return this.each(function(){
 			var i = 1;
+			var $svgParent = $( this );
 			var layersnapDiv = new Snap( this );
-			var svg = layersnapDiv.select( "svg" );
-			var bbox = svg.getBBox(); //bounding box, get coords and centre
+			var svg = layersnapDiv.select( svgEl );
+			var bbox = svg.getBBox(); //bounding box, get coords and center
 
-			layersnapDiv.selectAll( "svg > g[id]" ).forEach(function(elem){
-				var el = elem;
-				el.attr( "opacity", 0 );
-				var elID = el.attr( "id" );
-				var idDuration = elID.match( /(^|\s|_)duration[\-_]+([\d]+)/ );
-				var duration = 800;
+			layersnapDiv.selectAll( childGroups ).forEach(function(elem){
+				var ret = {
+					el: elem,
+					duration: 800,
+					bbox: bbox
+				};
+				// get settings from el ID
+				var elID = ret.el.attr( "id" );
+				// override duration if set
+				var idDuration = elID.match( regDuration);
 				if( idDuration ){
-					duration = parseFloat( idDuration[ 2 ] );
+					ret.duration = parseFloat( idDuration[ 2 ] );
 				}
-				var idDelay = elID.match( /(^|\s|_)delay[\-_]+([\d]+)/ );
-				var delay = ( duration * i - duration );
+				// override duration if set
+				var idDelay = elID.match( regDelay );
+				ret.delay = ( ret.duration * i - ret.duration );
 				if( idDelay ){
-					delay =  parseFloat( idDelay[ 2 ] );
+					ret.delay =  parseFloat( idDelay[ 2 ] );
 				}
-				setTimeout(function(){
-					if( elID.indexOf( "rotate-right" ) > -1 ){
-						el.attr( {transform: "r-30"} );
-						el.animate({ transform: "r0," + bbox.cx + ',' + bbox.cy, opacity: 1 }, duration, mina.easeOut );
+				$.each( transitions, function( name ){
+					if( elID.indexOf( name ) > -1 ){
+						ret.transition = name;
 					}
-					if( elID.indexOf( "rotate-left" ) > -1 ){
-						el.attr( {transform: "r30"} );
-						el.animate({ transform: "r0," + bbox.cx + ',' + bbox.cy, opacity: 1 }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "fade" ) > -1 ){
-						el.animate({ opacity: 1 }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "scale-up" ) > -1 ){
-						el.attr( {transform: "s.7"} );
-						el.animate({ opacity: 1, transform: "s1," + bbox.cx + ',' + bbox.cy }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "scale-down" ) > -1 ){
-						el.attr( {transform: "s1.3"} );
-						el.animate({ opacity: 1, transform: "s1," + bbox.cx + ',' + bbox.cy }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "pop" ) > -1 ){
-						el.attr( {transform: "s.7"} );
-						el.animate({ opacity: 1, transform: "s1," + bbox.cx + ',' + bbox.cy }, duration, mina.elastic );
-					}
-					if( elID.indexOf( "drift-up"  ) > -1){
-						el.attr( {transform: "translate(0,30)"} );
-						el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "drift-down"  ) > -1){
-						el.attr( {transform: "translate(0,-30)"} );
-						el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "drift-left"  ) > -1){
-						el.attr( {transform: "translate(30,0)"} );
-						el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "drift-right"  ) > -1){
-						el.attr( {transform: "translate(-30,0)"} );
-						el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "slide-up"  ) > -1){
-						el.attr( {transform: "translate(0," + bbox.height + ")"} );
-						el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "slide-down"  ) > -1){
-						el.attr( {transform: "translate(0," + -bbox.height + ")"} );
-						el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "slide-left"  ) > -1){
-						el.attr( {transform: "translate(" + bbox.width + ",0)"} );
-						el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
-					}
-					if( elID.indexOf( "slide-right"  ) > -1){
-						el.attr( {transform: "translate(" + -bbox.width + ",0)"} );
-						el.animate({ opacity: 1, transform: "translate(0,0)" }, duration, mina.easeOut );
-					}
-				}, delay );
+				} );
+				if( ret.transition ){
+					runTransition( ret );
+				}
 				i++;
 			});
 
 			// interactivity
-			var interactiveAttr = "data-layersnap-interact";
-			var activeGroupClass = "layersnap-toggle-active";
-			var activeGroupID = "activegroup";
-			var toggleClass = "layersnap-toggle-hide";
-			var toggleTriggerElementClass = "layersnap-toggle";
-			if( $( this ).is( "[" + interactiveAttr + "]" ) && $( this ).attr( interactiveAttr ) !== "bound" ){
+			if( $( this ).is( "[" + interactiveAttr + "]" ) && $( this ).attr( interactiveAttr ) !== interactivitySet ){
 
-				var $svgParent = $( this );
 				$svgParent
-					.attr( interactiveAttr, "bound" )
-					.bind( "tap", function( e ){
+					.attr( interactiveAttr, interactivitySet )
+					.bind( "click toggleElem", function( e ){
 						var $el = $( e.target ).closest( "g[id]" );
 						var elID = $el.attr( "id" );
 						if( elID ){
-							var toggleID = elID.match( /(^|\s|_)toggle\-([^\s_$]+)/ );
+							var toggleID = elID.match( regToggle );
 							if( toggleID.length ){
 								var $toggle = $( "#" + toggleID[ 2 ] );
 
@@ -127,8 +161,8 @@ SVG Build Animations
 
 				// trigger initial toggle if specified
 				$svgParent
-					.find( "g[id*='" + activeGroupID + "'" )
-					.trigger( "tap" );
+					.find( activeGroupSel )
+					.trigger( "toggleElem" );
 			}
 
 		});
