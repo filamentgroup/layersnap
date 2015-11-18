eaac31a932cf7c2bbc0cb6cc385896f066b1ad41
Scott Jehl <scottjehl@gmail.com>
2015-11-17 21:13:42 -0600

    fix regression with replay button causing interactive examples to no longer be clickable. replay button is now disabled in interactive svgs

--- dist/layersnap.js
+++ dist/layersnap.js
@@ -1,4 +1,4 @@
-/*! layersnap - v0.1.6 - 2015-10-12
+/*! layersnap - v0.1.6 - 2015-11-17
 * https://github.com/filamentgroup/layersnap
 * Copyright (c) 2015 Filament Group; Licensed MIT */
 (function($,w){
